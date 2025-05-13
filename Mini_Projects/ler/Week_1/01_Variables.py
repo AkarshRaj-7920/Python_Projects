d57b44377e9d7d45a318d494c09bed1f5ac84039
AkarshRaj-7920 <d36307920@gmail.com>
2025-05-14 01:42:46 +0530

    Some Of my mini projects

---
+++ Mini_Projects/ler/Week_1/01_Variables.py
@@ -0,0 +1,17 @@
+# Storing Freddy's Information
+
+Name_full = "Freddy Writer"
+print("name = ", Name_full)
+print(type(Name_full))
+
+Age = 32
+print("age = ",Age)
+print(type(Age))
+
+cgpa_12_grade = 5.887 # He just passed his 12th grade as Freddy Writer couden't write
+print("12th Grade CGPA = ",cgpa_12_grade)
+print(type(cgpa_12_grade))
+
+Height_feets = 4.2 # He was shortest guy in his whole school
+print("Height of Writter = ",Height_feets)
+print(type(Height_feets))
