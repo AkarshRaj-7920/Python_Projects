f60acc3415e1d0d24b780a2650ac1b09251993f0
AkarshRaj-7920 <d36307920@gmail.com>
2025-05-14 23:47:21 +0530

    Addes a new project on input

---
+++ Mini_Projects/ler/Week_1/03_user_input_demo.py
@@ -0,0 +1,13 @@
+# Taking user Information using Input and Typecasting 
+ 
+name__user = input("Enter your Good Name: ")
+age__user = int(input("Enter your Age: "))
+dob__month__user = int(input("Enter the month you increased weight of Earth(in numbers): ")) 
+height__user = float(input("Enter your Height(in fts): "))
+
+
+print ("\n -------- User Information --------")
+print ("Name: ", name__user)
+print ("Age: ", age__user)
+print ("Your Height:", height__user, "Feets")
+print ("Congrats you will successfully waste your ", age__user + 1, "years on", dob__month__user,"th Month of the upcoming year")
