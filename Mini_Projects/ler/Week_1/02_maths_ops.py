bcf2c6518da551dd2f0c4d10a24820ab2e413fcd
AkarshRaj-7920 <d36307920@gmail.com>
2025-05-14 16:06:44 +0530

    Added new mini project

---
+++ Mini_Projects/ler/Week_1/02_maths_ops.py
@@ -0,0 +1,9 @@
+# performing some mathematical operations
+
+a = 56
+b = 52
+
+print ("Sum of a & b", a+b)
+print ("Substration of a & b", a-b)
+print ("Multiplaction of a & b", a*b)
+print ("Division of a & b", a/b)
