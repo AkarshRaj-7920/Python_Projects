94e0e8479454c703a4c7580823a6dc1c470e3089
AkarshRaj-7920 <d36307920@gmail.com>
2025-05-16 01:52:07 +0530

    Build a marks analyzer check out

--- Mini_Projects/ler/Week_1/04_marks_analyzer.py
+++ Mini_Projects/ler/Week_1/04_marks_analyzer.py
@@ -11,6 +11,8 @@ _math_ = 80
 _sci_ = 80
 _sst_ = 80
 
+test_marks_overall = _eng_ + _2lang_ + _math_ + _sci_ + _sst_
+
 
 sub1_English = int(input("Enter your score in English out of 100: "))
 
@@ -33,13 +35,70 @@ if (0 <= sub1_English <= 100):
                     print("Done calculating your total overview")
                 else:
                     print ("Enter Correct Values and try again")
+                    exit()
             else:
                 print ("Enter Correct Values and try again")
+                exit()
         else:
             print ("Enter Correct Values and try again")
+            exit()
     else:
         print ("Enter correct Values and try again")
+        exit()
 else:
     print ("Enter correct Values and try again")
-    
+    exit()
 
+
+print("\n----------Score Card----------")
+'''
+Score Card includes 
+Total Marks, Average Marks, Percentage, Subjects passed, Overall result
+'''
+
+total_marks = sub1_English + sub2_IInd_lang + sub3_Maths + sub4_Science + sub5_SST
+print ("\nTotal Marks Obtained: ", total_marks,"\\", test_marks_overall)
+
+ovr_all_avg = total_marks/5
+print ("Average in 5 subjects: ", ovr_all_avg)
+
+per_centage = (total_marks/test_marks_overall) * 100
+print ("Percentage scored: ", per_centage)
+
+# Calculating over all result passed or failed
+
+print("\n-----Passed or Failed-----")
+if (sub1_English <= 35) and (sub2_IInd_lang <= 35) and (sub3_Maths <= 26) and (sub4_Science <= 26) and (sub5_SST <= 26):
+    print("\nOver all Result Failed")
+elif (35 < sub1_English <= 85) and (35 < sub2_IInd_lang <= 85) and (26 < sub3_Maths <= 65) and (26 < sub4_Science <= 65) and (26 < sub5_SST <= 65):
+    print("\nOver all Result Passed")
+elif (sub1_English > 85) and (sub2_IInd_lang > 85) and (sub3_Maths > 65) and (sub4_Science > 65) and (sub5_SST > 65):
+    print ("\nPassed with Grade A")
+else :
+    print("\nFailed")
+
+print ("\n-----Subectwise passed and Failed-----")
+if (sub1_English >= 35):
+    print("\nEnglish: Passed")
+else:
+    print("\nEnglish: Failed")
+     
+if (sub2_IInd_lang >= 35): 
+    print("IInd Language: Passed")
+else:
+    print("IInd Language: Failed")
+    
+if (sub3_Maths >= 26):
+    print("Maths: Passed")
+else:
+    print("Maths: Failed") 
+
+if (sub4_Science >= 26):
+    print("Science: Passed")
+else:
+    print("Science: Failed")
+
+if (sub5_SST >= 26):
+    print("Social Studies: Passed")
+else:
+    print("Social Studies: Failed")
