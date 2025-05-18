77897a3feb299bb2b325a5fd1048e252beed460d
Akarsh Raj <d36307920@gmail.com>
2025-05-18 20:17:52 +0530

    Smart Filter Printer

---
+++ Mini_Projects/ler/Week_1/08_smart_filter_printer.py
@@ -0,0 +1,26 @@
+# Smart filter Printer
+
+print ("""\nYou are going to be terminated in 100 seconds
+We have tracked you using python
+Stop me if u can""")
+
+for i in range (101):
+    if i == 3:
+        print("Hahaha run for your life")
+    
+    if i == 25:
+        pass
+    
+    if i == 50:
+        print("can't wait to get rid of u")
+        continue
+    if i == 81:
+        print("""\n\tYes I solved the bug in program
+        now boss going to give me reward, YESS!""")
+        print('''\tBoss: Sorry friend someone else did before you
+            your not getting the reward, now get out my face.''')
+        break
+    print (i)
+    
+
+
