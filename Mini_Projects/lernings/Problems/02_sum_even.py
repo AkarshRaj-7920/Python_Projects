6aca7e0409311a1a0fafa3a2d458e080e4ec074e
Akarsh Raj <d36307920@gmail.com>
2025-05-22 17:46:45 +0530

    Sum of Even numbers

---
+++ Mini_Projects/lernings/Problems/02_sum_even.py
@@ -0,0 +1,18 @@
+# Problem 2
+# Sum of all Even Numbers
+
+print("Find Sum of Positive Even Numbers \n")
+
+while True:
+    evn_num = input("Enter a Number: ")
+    if evn_num.isdigit():
+        evn_num = int(evn_num)
+        break
+    else:
+        print("\n\t---|Enter Positive Integers|---")
+    
+numbers = 0
+for i in range(2, evn_num + 1, 2):
+    numbers += i
+
+print("Sum =", numbers)
