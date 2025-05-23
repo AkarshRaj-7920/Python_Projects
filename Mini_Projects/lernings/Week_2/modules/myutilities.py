3d21479f392eddb8f7b998ba69453ebb773f3884
Akarsh Raj <d36307920@gmail.com>
2025-05-24 03:01:21 +0530

    Modules that are used in some tasks

---
+++ Mini_Projects/lernings/Week_2/modules/myutilities.py
@@ -0,0 +1,7 @@
+def greet(name):
+    return f"Hello, {name}! Welcome to the utility toolkit"
+
+def fact_torial(n):
+    import math
+    return math.factorial(n)
+
---
+++ Mini_Projects/lernings/Week_2/modules/test.py
@@ -0,0 +1,15 @@
+# This file is to test few unaware things
+
+# Testing Date and Time Module
+import datetime
+
+z = datetime.datetime.now()
+custom = z.strftime("%y-%m-%d %H-%M-%S")
+print(custom)
+
+
+# Testing Maths Module
+import math
+
+x = math.factorial(12)
+print(x)
