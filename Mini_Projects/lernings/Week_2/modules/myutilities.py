7e3f9de602642ca6a48313b1d33e2b78633d7c61
Akarsh Raj <d36307920@gmail.com>
2025-05-24 03:18:58 +0530

    Self-created modules for task 11

--- Mini_Projects/lernings/Week_2/modules/myutilities.py
+++ Mini_Projects/lernings/Week_2/modules/myutilities.py
@@ -1,7 +1,6 @@
 def greet(name):
-    return f"Hello, {name}! Welcome to the utility toolkit"
+    return f"Hello, {name}! Welcome to the Utility Toolkit"
 
 def fact_torial(n):
     import math
     return math.factorial(n)
-
