69fc56f512e30c5e3b209adeb77c06f5b1ec0b40
Akarsh Raj <d36307920@gmail.com>
2025-06-02 15:19:12 +0530

    List Toolkit

--- Mini_Projects/lernings/Week_3/Tasks/13_list_toolkit.py
+++ Mini_Projects/lernings/Week_3/Tasks/13_list_toolkit.py
@@ -8,21 +8,31 @@ while True:
         num = int(num)
         list1.append(num)
         print("Enter another number OR \nType 'End' to get results\n")
-    elif num == "End":
+    elif num.lower() == "end":
         print("\n-----|Your Results|-----\n")
         break
     else:
         print("Invalid Input")
 
+# Original List
+print(f"Original List: {list1}")
+
+# Squares of all the numbers
+list2 = [q**2 for q in list1]
+print(f"Squares of Numbers = {list2}")
+
+# Remove Duplitaces
+print(f"The list without repeating numbers is {set(list1)}")
+
 # Sorted List
 list1.sort()
 print(f"Sorted List:- {list1}")
+
+# Highest and Lowest value
+print(f"Highest Value {list1[-1]} \nSmallest Value {list1[0]}")
 
 # Sum of all Numbers
 num_sum = 0
 for i in list1:
     num_sum += i
 print(f"Sum of all Numbers = {num_sum}")
-
-
-
