1067abd0c902c47717ce750a4c24825f17dd8c5b
Akarsh Raj <d36307920@gmail.com>
2025-07-09 18:38:10 +0530

    Reverse Words Sentence

--- Mini_Projects/lernings/Code Polisher/Week_1/Day_2/01_reverse_words_sentence.py
+++ Mini_Projects/lernings/Code Polisher/Week_1/Day_2/01_reverse_words_sentence.py
@@ -1,9 +1,20 @@
 # Reverse Words in a Sentence
 
 # Functions
-def reverse_words_sentence(text: str) -> list[str]:
-    pass
+def reverse_words_sentence(text: str) -> str:
+    '''
+    Reverses the order of the words in a sentence while preserving esch word.
+    Args:
+        text (str): The input sentence.
 
+    Returns:
+        str: The sentence with word order reversed.
+    '''
+
+    words = text.split()
+    reversed_words = words[::-1]
+    return " ".join(reversed_words)
+    
 # Input
 def main():
     while True:
@@ -12,6 +23,9 @@ def main():
             print(" ○ Invalid Input. Please enter values again...")
         else:
             break
+
+    result = reverse_words_sentence(statement)
+    print(f"Reversed Statement: {result}")
     
 if __name__ == "__main__":
     main()
